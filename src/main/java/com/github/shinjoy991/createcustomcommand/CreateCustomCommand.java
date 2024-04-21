e2c45974a7fde2b3045bb1f53582c6bee47c20fc
ShinJoy991 <shinjoytestmail@gmail.com>
2024-04-21 13:15:26 +0700

    1.20.2 first push

---
+++ src/main/java/com/github/shinjoy991/createcustomcommand/CreateCustomCommand.java
@@ -0,0 +1,38 @@
+package com.github.shinjoy991.createcustomcommand;
+
+import com.github.shinjoy991.createcustomcommand.config.CreateJson;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import static com.github.shinjoy991.createcustomcommand.config.ReadConfig.readJsonValue;
+
+@Mod(CreateCustomCommand.MOD_ID)
+public class CreateCustomCommand {
+
+    public static final String MOD_ID = "createcustomcommand";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateCustomCommand() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("CreateCustomCommand PREINIT setting up...");
+        CreateJson.CreateJsonConfigFile();
+        readJsonValue(CreateJson.configFile);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("CreateCustomCommand hello from server starting");
+    }
+}
